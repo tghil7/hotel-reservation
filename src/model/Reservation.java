0d96f8d7d6477c19c4cacb776c3d8e3037716ccf
Anicet <tghil7@aol.com>
2021-09-18 16:39:04 -0500

    New methods added to the Reservation Service class

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -7,9 +7,47 @@ public class Reservation {
     private Date checkInDate;
     private Date checkOutDate;
 
-    @Override
-    public String toString(){
-       return "Customer: "  + customer.toString() + "Room" + room + " Check in date" + checkInDate + " check out date " + checkOutDate;
+    public Reservation(Customer customer, IRoom room, Date checkInDate, Date checkOutDate){
+        this.customer = customer;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
     }
 
+    public IRoom getRoom(){
+        return room;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Customer getCustomer(){
+        return customer;
+    }
+
+    public Date getCheckInDate(){
+        return checkInDate;
+    }
+
+    public Date getCheckOutDate(){
+        return checkOutDate;
+    }
+
+    public void setRoom(IRoom room) {
+        this.room = room;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    @Override
+    public String toString(){
+        return "Customer: "  + customer.toString() + "Room" + room + " Check in date" + checkInDate + " check out date " + checkOutDate;
+    }
 }
---
+++ src/service/ReservationService.java
@@ -0,0 +1,52 @@
+package service;
+
+import model.IRoom;
+import model.Room;
+import model.Reservation;
+import java.util.*;
+import model.Customer;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class ReservationService {
+
+    //Collection to store all rooms
+    private Map<String, Room> roomQueue  = new HashMap<String, Room>();
+
+    //Collection to store available rooms;
+    private Set<IRoom> availableRooms = new HashSet<IRoom>();
+
+    //Collection to store reservations.
+    private Set<Reservation> customerReservation = new HashSet<Reservation>();
+  public void addRoom(IRoom room){
+      roomQueue.put(room.getRoomNumber(), (Room) room);
+
+  }
+
+  public IRoom getARoom(String roomId){
+      return roomQueue.get(roomId);
+  }
+
+  public Reservation reserveARoom (Customer customer, IRoom room, Date checkInDate, Date checkOutDate ){
+      //Add the new reservation to the set of reservations.
+      Reservation reservation = new Reservation(customer, room, checkInDate, checkOutDate);
+      customerReservation.add(reservation);
+      return reservation;
+
+  }
+
+  public Collection<IRoom> findRooms (Date checkInDate, Date checkOutDate) {
+      for (IRoom room : roomQueue.values()) {
+          if (room.isFree()) {
+              availableRooms.add(room);
+          }
+      }
+      return availableRooms;
+  }
+
+  public Collection<Reservation> getCustomersReservation(Customer customer){
+       return customerReservation;
+  }
+}
